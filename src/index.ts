5cdb437f43812b61e2742d008ab121923d708a96
Matt Bishop <matt@withinfocus.com>
2025-12-20 09:26:41 -0500

    Bump version

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ async function runServer(): Promise<void> {
     server = new Server(
       {
         name: 'The Blue Alliance MCP Server',
-        version: '1.0.0',
+        version: '1.0.1',
       },
       {
         capabilities: {
